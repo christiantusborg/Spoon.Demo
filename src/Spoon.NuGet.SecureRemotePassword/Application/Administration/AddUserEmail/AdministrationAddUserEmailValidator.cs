3226935e1f88fa9e63fba2c0dc760a0d7d580d06
Christian Tusborg <gitlab.com@terbium.dk>
2023-04-13 23:31:12 +0200

    Code in AdministrationAddUserEmailCommand

--- src/Spoon.NuGet.SecureRemotePassword/Application/Administration/AddUserEmail/AdministrationAddUserEmailValidator.cs
+++ src/Spoon.NuGet.SecureRemotePassword/Application/Administration/AddUserEmail/AdministrationAddUserEmailValidator.cs
@@ -1,27 +1,10 @@
 ﻿namespace Spoon.NuGet.SecureRemotePassword.Application.Administration.AddUserEmail;
 
-using FluentValidation;
+using Core.Validation;
 
 /// <summary>
 ///     Validator for <see cref="AdministrationAddUserEmailCommand" />.
 /// </summary>
-public sealed class AdministrationAddUserEmailValidator : AbstractValidator<AdministrationAddUserEmailCommand>
+public sealed class AdministrationAddUserEmailValidator : BaseValidator<AdministrationAddUserEmailCommand>
 {
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="AdministrationAddUserEmailValidator" /> class.
-    /// </summary>
-    public AdministrationAddUserEmailValidator()
-    {
-        this.RuleFor(x => x.UserId)
-            .NotEmpty()
-            .WithMessage($"{this.GetType().Name}_IsEmpty_UserId")
-            .NotEqual(Guid.Empty)
-            .WithMessage($"{this.GetType().Name}_InvalidGuid_UserId");
-
-        this.RuleFor(x => x.Email)
-            .NotEmpty()
-            .WithMessage($"{this.GetType().Name}_IsEmpty_Email")
-            .EmailAddress()
-            .WithMessage($"{this.GetType().Name}_InvalidEmailFormat_Email");
-    }
 }
